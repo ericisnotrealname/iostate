f31c795fe0ec76517589a2810402823e68bb46cc
Ye.Lin <Ye.Lin@cnexlabs.com>
2022-08-10 17:28:46 +0800

    modified:   lib/graph.py

--- lib/graph.py
+++ lib/graph.py
@@ -1,3 +1,18 @@
-from pylab import *
+from matplotlib import pyplot as plt
+
+
+class GRAPH_FIO:
+    def __init__(self, title_name:str) -> None:
+        self.x_runtime = list()
+        self.y_bw = list()
+        self.y_iops = list()
+        plt.title(title_name)
+
+    def generate_chart(self):
+        plt.xlabel("runtime/msec")
+        if len(self.x_runtime) == len(self.y_bw):
+            plt.plot(self.x_runtime, self.y_bw, label="bw", color="#00FF00", marker=".", linestyle="-")
+        if len(self.x_runtime) == len(self.y_iops):
+            plt.plot(self.x_runtime, self.y_iops, label="iops", color="#00FF00", marker=".", linestyle="--")
 
 
